2754ea0910d4a287b1258ce48ec1398a6e02dab7
cwllight8 <cwllight8@gmail.com>
2020-02-14 18:22:15 -0700

    scripts n stuff, just tesing

---
+++ Recipies/Script1.sql
@@ -0,0 +1,23 @@
+﻿create table Person(
+	PersonId int not null primary key,
+	FistName varchar(50) not null,
+	LastName varchar(50) not null,
+	Occupation varchar(100) null
+)
+go
+
+create table Bulldogs(
+	BulldogId int not null primary key,
+	[Name] varchar(50) not null,
+	Nickname varchar(50) null,
+	DoB date not null check(DoB <= getdate()),
+	Age int not null check(Age >= 0),
+	PrimaryPersonId int not null references Person(PersonId) --refers to valid PersonId
+)
+go
+
+create table Colors(
+	ColorId int not null primary key,
+	[Name] varchar(50) not null
+)
+go
