8608a6b602e10dd0d0876928df6d78a0a0d1fd8f
cwllight8 <cwllight8@gmail.com>
2020-03-04 17:48:09 -0700

    got recipe creation working

--- EZRecipize/Controllers/RecipeController.cs
+++ EZRecipize/Controllers/RecipeController.cs
@@ -80,21 +80,23 @@ namespace EZRecipize.Controllers
             })
         };
         private static Recipe recipe;
+        private static RecipeStep recipeStep;
 
         public IActionResult Recipes()
         {
             return View(recipes);
         }
-        
-        public IActionResult RecipeDetails(Recipe r)
+
+        //public IActionResult RecipeDetails(Recipe r)
+        //{
+        //    return View(r);
+        //}
+
+        public IActionResult RecipeDetails(int recipeId)
         {
+            Recipe r = recipes.Where(r => r.Id == recipeId).FirstOrDefault();
             return View(r);
         }
-        
-        //public IActionResult RecipeDetails(int recipeNum)
-        //{
-        //    return View(recipes.ElementAt(recipeNum));
-        //}
 
         public IActionResult RecipeCooking()
         {
@@ -112,12 +114,21 @@ namespace EZRecipize.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Recipe recipeTemp)
+        public IActionResult Create(Recipe recipeTemp, bool addingStep)
         {
-            if (ModelState.IsValid)
+            if (ModelState.IsValid && !addingStep && recipe.Steps.Count != 0)
             {
+                if(recipe.Steps != null && recipe.Steps.Count > 0)
+                {
+                    recipe.Steps.Last().SeperateListStrings();
+                }
+                recipeTemp.Steps = recipe.Steps;
                 recipes.Add(recipeTemp);
                 return View("RecipeDetails", recipeTemp);
+            }
+            else if(addingStep)
+            {
+                return RedirectToAction("CreateStep", recipeTemp);
             }
             else
             {
@@ -125,22 +136,113 @@ namespace EZRecipize.Controllers
             }
         }
 
-        public IActionResult CreateStep(Recipe r)
+        public IActionResult CreateStep(Recipe r, bool active = false)
         {
+
+            if (!active)
+            {
+                recipeStep = new RecipeStep();
+            }
+            
             recipe.RecipeName = r.RecipeName;
             recipe.RecipeDescription = r.RecipeDescription;
+            if(recipeStep.RequiredCookware == null)
+            {
+                recipeStep.RequiredCookware = new List<string>();
+            }
+            if(recipeStep.SuggestedCookware == null)
+            {
+                recipeStep.SuggestedCookware = new List<string>();
+            }
+            return View(recipeStep);
+        }
+
+        [HttpPost]
+        public IActionResult CreateStep(RecipeStep s, bool addingIngredient)
+        {
+            if (ModelState.IsValid && !addingIngredient)
+            {
+                recipeStep.SeperateListStrings();
+                recipeStep.Instructions = s.Instructions;
+                recipeStep.IsTimer = s.IsTimer;
+                recipeStep.Timer = s.Timer;
+                if (s.RequiredCookware != null && s.RequiredCookware.Count > 0 && s.RequiredCookware.ElementAt(0) != null && recipeStep.RequiredCookware != null && s.RequiredCookware.Count != recipeStep.RequiredCookware.Count)
+                {
+                    for (int i = 0; i < s.RequiredCookware.Count; i++)
+                    {
+
+                        recipeStep.RequiredCookware.Add(s.RequiredCookware.ElementAt(i));
+                    }
+                }
+                if (s.SuggestedCookware != null && s.SuggestedCookware.Count > 0 && s.SuggestedCookware.ElementAt(0) != null && recipeStep.SuggestedCookware != null && s.SuggestedCookware.Count != recipeStep.SuggestedCookware.Count)
+                {
+                    for (int i = 0; i < s.SuggestedCookware.Count; i++)
+                    {
+                        recipeStep.SuggestedCookware.Add(s.SuggestedCookware.ElementAt(i));
+                    }
+                }
+
+                recipe.Steps.Add(recipeStep);
+                return View("Create", recipe);
+            }
+            else if(addingIngredient)
+            {
+                recipeStep.Instructions = s.Instructions;
+                recipeStep.IsTimer = s.IsTimer;
+                recipeStep.Timer = s.Timer;
+                recipeStep.TempRequiredCookware = s.TempRequiredCookware;
+                if (s.RequiredCookware != null && s.RequiredCookware.Count > 0 && s.RequiredCookware.ElementAt(0) != null && recipeStep.RequiredCookware != null && s.RequiredCookware.Count != recipeStep.RequiredCookware.Count)
+                {
+                    for (int i = 0; i < s.RequiredCookware.Count; i++)
+                    {
+                        
+                        recipeStep.RequiredCookware.Add(s.RequiredCookware.ElementAt(i));
+                    }
+                }
+                recipeStep.TempSuggestedCookware = s.TempSuggestedCookware;
+                if (s.SuggestedCookware != null && s.SuggestedCookware.Count > 0 && s.SuggestedCookware.ElementAt(0) != null && recipeStep.SuggestedCookware != null && s.SuggestedCookware.Count != recipeStep.SuggestedCookware.Count)
+                {
+                    for (int i = 0; i < s.SuggestedCookware.Count; i++)
+                    {
+                        recipeStep.SuggestedCookware.Add(s.SuggestedCookware.ElementAt(i));
+                    }
+                }
+                //recipeStep = s;
+
+                return RedirectToAction("CreateIngredient", recipeStep);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult CreateIngredient(RecipeStep s)
+        {
+            //recipeStep = new RecipeStep();
+
+            if (recipeStep.Ingredients == null)
+            {
+                recipeStep.Ingredients = new List<Ingredient>();
+            }
+
+            recipeStep.Ingredients.Add(new Ingredient());
+
             return View();
         }
 
         [HttpPost]
-        public IActionResult CreateStep(RecipeStep step)
+        public IActionResult CreateIngredient(Ingredient i)
         {
             if (ModelState.IsValid)
             {
-                recipe.Steps.Add(step);
-                //return Create(true);
-                //return RedirectToAction("Create", true);
-                return View("Create", recipe);
+
+                recipeStep.Ingredients.Last().Name = i.Name;
+                recipeStep.Ingredients.Last().Amount = i.Amount;
+                recipeStep.Ingredients.Last().Unit = i.Unit;
+                recipeStep.Ingredients.Last().SpecialInstructions = i.SpecialInstructions;
+
+                return View("CreateStep", recipeStep);
             }
             else
             {
